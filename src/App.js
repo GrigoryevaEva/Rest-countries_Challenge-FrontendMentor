1b99849f1f91bf15727c26bc6d8e93df00e2a5a3
Eva <grigoryeva.eva.2704@mail.ru>
2024-03-19 14:31:45 +0300

    base project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-
-import Layout from "./pages/Layout";
-import MainPage from './pages/MainPage';
-import CountryPage from './pages/CountryPage';
-import ErrorPage from './pages/ErrorPage';
-
-import { loaderMainPage, loaderCountryPage } from './api/Loader';
-
-import "./scss/test.scss";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <MainPage />,
-        loader: loaderMainPage
-      },
-      {
-        path: "/:countryCode",
-        element: <CountryPage />,
-        loader: loaderCountryPage
-      },
-    ]
-  },
-]);
-
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div>
+    </div>
+  );
 }
 
 export default App;
