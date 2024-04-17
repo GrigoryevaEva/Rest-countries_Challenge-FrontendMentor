30678e718bba97b73e259c7f2d790e80cc42065e
Eva <grigoryeva.eva.2704@mail.ru>
2024-04-17 15:06:21 +0300

    refactoring branches

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import Layout from "./pages/Layout";
+import MainPage from './pages/MainPage';
+import CountryPage from './pages/CountryPage';
+import ErrorPage from './pages/ErrorPage';
+
+import { loaderMainPage, loaderCountryPage } from './api/Loader';
+
+import "./scss/test.scss";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />,
+        loader: loaderMainPage
+      },
+      {
+        path: "/:countryCode",
+        element: <CountryPage />,
+        loader: loaderCountryPage
+      },
+    ]
+  },
+]);
+
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
