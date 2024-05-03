099abd6033dbbfe04dba9d13ed3c859d0db18393
Eva <grigoryeva.eva.2704@mail.ru>
2024-05-03 16:31:26 +0300

    correction mistake in selectors

--- src/entities/country/model/selectors.js
+++ src/entities/country/model/selectors.js
@@ -1,7 +1,7 @@
 export const selectData = state => state.country.country
 
-export const selectStatus = state => state.countries.status
+export const selectStatus = state => state.country.status
 
-export const selectError = state => state.countries.error
+export const selectError = state => state.country.error
 
-export const selectCountryBorders = state => state.countries.countryBorders
+export const selectCountryBorders = state => state.country.countryBorders
